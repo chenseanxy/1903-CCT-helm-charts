30b2e7029e925cde097d821ffd8d6ff28dffdaf8
chenxy <git@chenxy.tech>
2019-06-12 22:41:31 +0800

    Cleanup after project deadline

---
+++ helm-deploy.bat
@@ -0,0 +1,6 @@
+helm install --name kfk ./kafka
+helm install --name redis ./redis
+helm install --name hbase-zk ./zookeeper
+helm install --name hbase ./hbase
+helm install --name ccproject ./ccproject
+
